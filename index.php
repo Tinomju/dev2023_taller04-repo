d5a2a666d403cfb466c3ab051c52b3f7cdea7984
Martin Gonzalez <martinjaviergonzalez_2004@hotmail.com>
2023-10-23 14:49:46 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <title>Validación de Formularios</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/index.js"></script>
+</head>
+<body>
+  <form id="formulario">
+    <table cellspacing="2" cellpadding="2" border="1">
+      <tr>
+        <td align="right">Nombre de Usuario</td>
+        <td><input type="text" id="usuario" /></td>
+      </tr>
+      <tr>
+        <td align="right">Email</td>
+        <td><input type="email" id="email" /></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="submit" value="Enviar" id="btn-enviar" /></td>
+      </tr>
+    </table>
+  </form>
+</body>
+<script src="js/bootstrap.bundle.min.js"></script>
+</html>
